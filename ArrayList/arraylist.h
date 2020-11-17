f3827d548e7f0de583cfb4f2a2e716e856ff8b31
Zenfection <kietgolx65234@gmail.com>
2020-11-17 22:48:43 +0700

    update

---
+++ ArrayList/arraylist.h
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#define Max_length 50
+typedef struct{
+    int Elements[Max_length];
+    int Size;
+}List;
+void makeNullList(List *L);                 //* hàm tạo danh sách rỗng
+void displayList(List L);                   //* hàm hiện thị danh sách
+void InputList(int n,List *L);              //* hàm thêm n phần tử vào danh sách 
+void deleteListbyID(int x, List *L);        //* hàm xoá phần tử x trong danh sách
+void deleteListbyPos(int p,List *L);        //* hàm xoá vị trí p trong danh sách
+int memberList(int x,List L);               //* hàm kiểm tra x có trong danh sách
+int locateList(int x,List L);               //* hàm trả về vị trí đầu tiên của x trong danh sách
+void optimizeList(List *L);                 //* hàm tối ưu danh sách (1 2 1 -> 1 2)
+void insertList(int x,int p,List *L);       //* hàm thêm x tại vị trí p trong danh sách
+void mergeList(List L1,List L2,List *L);    //* gộp 2 danh sách vào 1 danh sách
+void filter_evenNumber(List L1, List *L);   //* lọc phần tử chẳn qua 1 danh sách
+void filter_oddNumber(List L1, List *L);    //* lọc phần tử lẻ qua 1 danh sách
+double averageList(List L);                 //* tính tổng các phần tử trong danh sách
+
+void makeNullList(List *L){
+    L->Size = 0;
+}
+void displayList(List L){
+    for (int i = 0; i < L.Size; i++){
+        printf("%d ",L.Elements[i]);
+    }
+}
+void deleteListbyPos(int p,List *L){
+    for(int i = p; i < L->Size - 1; i++){
+        L->Elements[i] = L->Elements[i+1];
+    }
+    L->Size--;
+}
+void deleteListbyID(int x, List *L){
+    for(int i = 0; i < L->Size; i++){
+        if(L->Elements[i] == x){
+            deleteListbyPos(i,L);
+        }
+    }
+}
+void InputList(int n,List *L){
+    makeNullList(L);
+    for(int i = 0; i < n ; i++){
+        scanf("%d",&L->Elements[i]);
+        L->Size++;
+    }
+}
+int member(int x,List L){
+    for(int i = 0; i < L.Size; i++){
+        if(L.Elements[i] == x){
+            return 1;
+        }
+    }
+    return 0;
+}
+int locateList(int x,List L){
+    for(int i = 0 ; i < L.Size ; i++){
+        if(L.Elements[i] == x){
+            return i;
+        }
+    }
+    return -1;
+}
+void insertList(int x,int p,List *L){
+    if(L->Size == Max_length){
+        printf("Full List!!!");
+    }
+    else if(p < 0 || p > L->Size){
+        printf("Position Invalid!!!");
+    }
+    else{
+        for(int i = L->Size; i >= p ; i--){
+            L->Elements[i]=L->Elements[i-1];
+        }
+        L->Size++;
+        L->Elements[p] = x;
+    }
+}
+void optimizeList(List *L){
+    for(int i = 0; i < L->Size - 1; i++){
+        for(int j=i+1 ;j < L->Size;j++){
+            if(L->Elements[i] == L->Elements[j]){
+                deleteListbyPos(j,L);
+            }
+        }
+    }
+}
+void mergeList(List L1,List L2,List *L){
+    for(int i = 0 ; i < L1.Size ; i++){
+        insertList(L1.Elements[i],i,L);
+    }
+    for(int i = 0 ; i < L2.Size ; i++){
+        insertList(L2.Elements[i],i+L1.Size,L);
+    }
+}
+void filter_evenNumber(List L1,List *L){
+    int k = 0;
+    for(int i = 0 ; i < L1.Size; i++){
+        if(L1.Elements[i] % 2 == 0){
+            insertList(L1.Elements[i],k,L);
+            k++;
+        }
+    }
+}
+void filter_oddNumber(List L1,List *L){
+    int k = 0;
+    for (int i = 0; i < L1.Size; i++){
+        if(L1.Elements[i] % 2 != 0){
+            insertList(L1.Elements[i],k,L);
+            k++;
+        }
+    }
+    
+}
+double averageList(List L){
+    double result = 0;
+    for (int i = 0; i < L.Size; i++){
+        result += L.Elements[i];
+    }
+    return result/L.Size;
+}
---
+++ ArrayList/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "arraylist.h"
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void taoRong(List *L){
+    
+}
