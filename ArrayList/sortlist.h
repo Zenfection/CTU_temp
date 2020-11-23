e883e6b0df3e20d2d12db91bab095c710f3b6619
Zenfection <kietgolx65234@gmail.com>
2020-11-23 18:38:07 +0700

    update

--- ArrayList/sortlist.h
+++ ArrayList/sortlist.h
@@ -1,13 +1,14 @@
-void swap(int *a,int *b);                   //hoán vị 2 số
-void interchangeSort(List *L);              //Sắp xếp trực tiếp
-void selectionSort(List *L);                //Sắp xếp chọn
-void bubbleSort(List *L);                   //Sắp xếp nổi bọt
-void insertionSort(List *L);                //Sắp xếp chèn
-void heaplify(int i,int n,List *L);         //Biến mảng thành cây nhị phân heap
-void heapSort(List *L);                     //Sắp xếp cây nhị phân
-void shellSort(List *L);                    //Sắp xếp shell (cải tiến sắp xếp chèn)
-void quickSort(List *L);
-void mergeSort(List *L);
+void swap(int *a,int *b);                             //hoán vị 2 số
+void interchangeSort(List *L);                        //Sắp xếp trực tiếp
+void selectionSort(List *L);                          //Sắp xếp chọn
+void bubbleSort(List *L);                             //Sắp xếp nổi bọt
+void insertionSort(List *L);                          //Sắp xếp chèn
+void heaplify(int i,int n,List *L);                   //Biến mảng thành cây nhị phân heap
+void heapSort(List *L);                               //Sắp xếp cây nhị phân
+void shellSort(List *L);                              //Sắp xếp shell (cải tiến sắp xếp chèn)
+void quickSort(List *L,int start,int end);            //Sắp xếp nhanh
+void merge(List *L,int left,int mid,int right);       //gộp mảng cho sắp xếp gộp
+void mergeSort(List *L,int start,int end);           //Sắp xếp gộp
 
 void swap(int *a,int *b){
     int temp = *a;
@@ -86,7 +87,8 @@ void heapSort(List *L){
 }
 void shellSort(List *L){
     int x,pos;
-    for(int h = L->Size / 2 ; h > 0 ; h /= 2){
+    int divide = 2; // có thể thay đổi vách chia
+    for(int h = L->Size / divide; h > 0 ; h /= divide){
         for(int i = h ; i < L->Size ; i++){
             x = L->Elements[i];
             pos = i - h;
@@ -96,5 +98,72 @@ void shellSort(List *L){
             }
             L->Elements[pos+h] = x;
         }
+    }
+}
+void quickSort(List *L,int start,int end){
+    int pivot = L->Elements[(start + end) / 2]; // tuỳ chọn pivot
+    int left = start;
+    int right = end;
+    while(left <= right){
+        while(L->Elements[left] < pivot){
+            left++;
+        }
+        while(L->Elements[right] > pivot){
+            right--;
+        }
+        if(left <= right){
+            swap(&L->Elements[left],&L->Elements[right]);
+            left++;
+            right--;
+        }
+    }
+    if(right > start){
+        quickSort(L,start,right);
+    }
+    if(left < end){
+        quickSort(L,left,end);
+    }
+}
+void merge(List *L,int left,int mid,int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int M1[n1],M2[n2];
+    for (int i = 0; i < n1; i++){
+        M1[i] = L->Elements[left + i];
+    }
+    for (int i = 0; i < n2; i++){
+        M2[i] = L->Elements[mid + 1 + i];
+    }
+    int i = 0;// mảng R
+    int j = 0;// mảng Q
+    int k = left;
+    while (i < n1 && j < n2){
+        if (M1[i] <= M2[j]){
+            L->Elements[k] = M1[i];
+            i++;
+        }
+        else{
+            L->Elements[k] = M2[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1){
+        L->Elements[k] = M1[i];
+        i++;
+        k++;
+    }
+    while (j < n2){
+        L->Elements[k] = M2[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(List *L,int start,int end){
+    if(start < end){
+        int half = start + ((end - start) / 2);
+        mergeSort(L,start,half);
+        mergeSort(L,half + 1,end);
+        merge(L,start,half,end);
     }
 }
