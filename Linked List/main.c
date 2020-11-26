ccaa68f0608dcafad0575cd6e5227e58d4fef0a5
Zenfection <kietgolx65234@gmail.com>
2020-11-26 10:59:55 +0700

    update

--- Linked List/main.c
+++ Linked List/main.c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]){
     List L;
     makeNullList(&L);
     inputList(5,&L);
-    interchangeSort(&L);
+    printf("%d ",binarySearch(2,L));
     displayList(L);
     return 0;
 }
