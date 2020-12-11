ca66b068e50f9e4c7addc3232b6ca89069e702ca
Zenfection <kietgolx65234@gmail.com>
2020-12-11 13:34:00 +0700

    uopdate

--- Tree/main.c
+++ Tree/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include "treeBST.h"
 int main(int argc, char const *argv[]){
-    Tree T = khoitao();
+    Tree T;
+    makeNullTree(&T);
+    insertTree(27,&T);
+    insertTree(12,&T);
+    insertTree(40,&T);
+    insertTree(4,&T);
+    insertTree(20,&T);
+    insertTree(34,&T);
+    insertTree(30,&T); 
+    insertTree(50,&T); 
+    insertTree(60,&T);
+    NLR(T);
+    printf("\n");
+    LRN(T);
+    deleteTree(12,&T);
+    
     return 0;
 }
 
--- Tree/treeBST.h
+++ Tree/treeBST.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "stdlib.h"
+#include <stdlib.h>
 struct Node{
     int data;
     struct Node *left;
@@ -13,6 +13,8 @@ Tree createTree();                          //* tạo Tree
 void NLR(Tree root);                        //* duyệt tiền tự
 void LNR(Tree root);                        //* duyệt trung tự
 void LRN(Tree root);                        //* duyệt hậu tự
+
+
 void makeNullTree(Tree *root){
     (*root) = NULL;
 }
@@ -69,4 +71,81 @@ void LRN(Tree root){
         LRN(root->right);
         printf("%d ",root->data);
     }
+}
+
+Tree getPrevious(int x,Tree root){
+    Tree t;
+    if(root != NULL){
+        getPrevious(x,root->left);
+        t = root;
+        if(t->data == x){
+            return t;
+        }
+        getPrevious(x,root->right);
+    }
+}
+
+Tree searchNode(int x,Tree root){
+    if(root != NULL){
+        if(root->data == x){
+            return root;
+        }
+        else if(root->data > x)
+            return searchNode(x,root->left);
+        else
+            return searchNode(x,root->right);
+    }
+    else{
+        return NULL;
+    }
+}
+void freeTree(Tree *root){
+    Tree temp = *root;
+    if(temp == NULL){
+        return;
+    }
+    freeTree(&temp->left);
+    freeTree(&temp->right);
+    free(temp);
+}
+void searchStandFor(Tree *p,Tree *q){
+    Tree temp1 = *p;
+    Tree temp2 = *q;
+    if(temp2->left != NULL){
+        searchStandFor(&temp1,&temp2->left);
+    }
+    else{
+        temp1->data = temp2->data;
+        temp1 = temp2;
+        temp2 = temp2->right;
+    }
+}
+int deleteTree(int x,Tree *root){
+    Tree temp = *root;
+    if(root == NULL){
+        return 0;
+    }
+    if(temp->data > x){
+        return deleteTree(x,&temp->left);
+    }
+    if(temp->data < x){
+        return deleteTree(x,&temp->right);
+    }
+    Tree p = *root;
+    if(temp->left == NULL){
+        temp = temp->right;
+    }
+    else{
+        if(temp->right == NULL)
+            temp = temp->left;
+        else
+            searchStandFor(&p,&temp->right);
+    }
+    if(p->right ->right != NULL){
+        p->right = p->right->right;
+    }
+    else{
+        p->right = NULL;
+    }
+    return 1;
 }
