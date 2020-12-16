e00419c52af0438d13ff67a363ca07edfeaaeef7
Zenfection <kietgolx65234@gmail.com>
2020-12-16 13:21:58 +0700

    update

--- CT177-Cau truc du lieu/Linked List/main.c
+++ CT177-Cau truc du lieu/Linked List/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include "./Library/linkedList.h"
-#include "./Library/sortList.h"
-#include "./Library/searchList.h"
+//#include "linkedList.h"
+//#include "sortList.h"
+//#include "searchList.h"
+#include "sentenceList.h"
 int main(int argc, char const *argv[]){
-    List L;
-    makeNullList(&L);
-    inputList(5,&L);
-    insertionSort(&L);
-    displayList(L);
+    
     return 0;
 }
